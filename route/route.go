6c9f0bcdf54ac6995c786ea0dafb8560b2477e31
Minh Nam <duongminhnam99@gmail.com>
2022-05-06 00:27:26 +0700

    [Finish] finish coding

--- route/route.go
+++ route/route.go
@@ -13,6 +13,9 @@ func InitRoutes() {
 
 	apiRoute := r.PathPrefix("/api/").Subrouter()
 	apiRoute.HandleFunc("/v1/issue", rest_issues.Create).Methods("POST")
+	apiRoute.HandleFunc("/v1/issue", rest_issues.Find).Methods("GET")
+	apiRoute.HandleFunc("/v1/issue", rest_issues.Delete).Methods("DELETE")
+	apiRoute.HandleFunc("/v1/issue", rest_issues.Update).Methods("PUT")
 
 	log.Fatal(http.ListenAndServe(":6868", r))
 }
